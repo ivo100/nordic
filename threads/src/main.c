853dea0dbcf867e1387d2a76b9d67cd55004ba6f
Ivo Stoyanov <ivo@skyway-consulting.com>
2023-03-18 17:54:52 -0700

    time slice

---
+++ threads/src/main.c
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2017 Linaro Limited
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+
+#define STACKSIZE 1024
+
+#define THREAD0_PRIORITY 7
+#define THREAD1_PRIORITY 7
+
+void thread0(void)
+{
+	while (1) {
+          printk("Hello, I am thread0\n");
+		  //k_msleep(2000);
+	}
+}
+
+void thread1(void)
+{
+	while (1) {
+          printk("Hello, I am thread1\n");
+		  //k_msleep(1000);
+	}
+}
+
+K_THREAD_DEFINE(thread0_id, STACKSIZE, thread0, NULL, NULL, NULL,
+		THREAD0_PRIORITY, 0, 0);
+K_THREAD_DEFINE(thread1_id, STACKSIZE, thread1, NULL, NULL, NULL,
+		THREAD1_PRIORITY, 0, 0);
+
